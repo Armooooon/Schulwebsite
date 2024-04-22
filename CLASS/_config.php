ce5568f1878314f826addebbcad215b44fb75dfe
Aron <halaoui.aron@gmail.com>
2024-04-22 21:10:31 +0200

    Added _config.php and DB.php

---
+++ CLASS/_config.php
@@ -0,0 +1,14 @@
+<?php
+//error_reporting(0);
+error_reporting(E_ALL);
+
+define('ROOT', 'http://127.0.0.1/schulwebsite');
+
+
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '1234');
+define('DB_NAME', 'Pestalozzi');
+
+define('DEBUG', true);
+?>
