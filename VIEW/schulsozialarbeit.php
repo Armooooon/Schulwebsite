873617ea1b596eaccc8b8d727f94cb37706b1c0d
Michel Blecken <122364575+Michi40469@users.noreply.github.com>
2024-04-23 13:48:57 +0200

    Update PHP file: add header text for "Schulsozialarbeit" page
    The changes in this commit include:
    - removal of three empty lines at the beginning of the file
    - addition of a div element with a h1

--- VIEW/schulsozialarbeit.php
+++ VIEW/schulsozialarbeit.php
@@ -1,5 +1,78 @@
 <?php Layout::Header("Schulsozialarbeit"); ?>
-
-
-
+<div class="container" style="display: block;">
+    <div class=headline_ssa_mb style="padding-top: 80px;">
+        <h1>Schulsozialarbeit an der Pestalozzi-Schule</h1>
+    </div>
+    <div class="angebot_ssa_mb">
+        <h2>Schulsozialarbeit ist ein Angebot für:</h2>
+        <ul>
+            <li>Schüler</li>
+            <li>Lehrer</li>
+            <li>Eltern/ Sorgeberechtigte</li>
+        </ul>
+    </div>
+    <div class="konflikt_ssa_mb">
+        <h2>Schulsozialarbeit bietet Unterstützung & Beratung bei Konflikten und Problemen der Schüler/innen:</h2>
+        <ul>
+            <li>in der Schule (z. B. schlechte Noten, Mobbing und Ausgrenzung, etc.)</li>
+            <li>in der Familie (z. B. Konflikte mit den Eltern, Trennung & Scheidung der Eltern, etc.)</li>
+            <li>mit sich selbst (z. B. Aggressionen, Wut, Ängste, Einsamkeit, Schuldgefühle, Ritzen, etc.)</li>
+            <li>im Freundeskreis</li>
+            <li>bei der Berufsorientierung/ beim Übergang Schule-Beruf (z. B. Fragen zur Bewerbung)</li>
+            <li>im sozialen Umfeld</li>
+        </ul>
+    </div>
+    <div class="gruppen_ssa_mb">
+        <h2>Schulsozialarbeit bietet Angebote für Gruppen und Klassen an:</h2>
+        <ul>
+            <li>Arbeit an der Klassendynamik/ Förderung der Klassengemeinschaft</li>
+            <li>Förderung von sozialen Kompetenzen/ Stärkung der Regelakzeptanz</li>
+            <li>Präventions- und Projektarbeit (z. B. zu den Themen Gewalt/ Mobbing, Respekt, gewaltfreie Kommunikation,
+                Demokratie, Sucht etc.)</li>
+            <li>Angebote im Rahmen der Berufsorientierung</li>
+            <li>Krisenintervention</li>
+        </ul>
+    </div>
+    <div class="entwicklung_ssa_mb">
+        <h2>Schulsozialarbeit wirkt an der Entwicklung der Schulkultur mit:</h2>
+        <ul>
+            <li>regelmäßiger Austausch mit der Schulleitung</li>
+            <li>Mitarbeit in schulischen Gremien (Teamsitzungen, Gesamtkonferenzen, Klassenkonferenzen etc.)</li>
+            <li>Mitarbeit in Arbeitsgemeinschaften</li>
+            <li>Mitarbeit am Schulprogramm</li>
+        </ul>
+    </div>
+    <div class="raum_ssa_mb">
+        <h2>Ich biete Zeit & Raum für:</h2>
+        <ul>
+            <li>Rückhalt</li>
+            <li>Rückhalt</li>
+            <li>die gemeinsame Suche nach Lösungen
+            </li>
+        </ul>
+    </div>
+    <div class="arbeite_ssa_mb">
+        <h2>Wie arbeite ich?</h2>
+        <ul>
+            <li>nach dem Prinzip der Freiwilligkeit, d. h. alle Gespräche & Aktivitäten sind freiwillig</li>
+            <li>nach dem Prinzip der Vertraulichkeit, d. h. ich bin an die Schweigepflicht gebunden</li>
+            <li>mit vielen Kooperationspartnern zusammen, ggf. Koordination von Maßnahmen und Hilfen (z. B.
+                Erziehungsberatung, Therapeuten, Sucht- und Jugendberatung, Jugendamt, Polizei, Ämter etc.)</li>
+            <li>im Sozialraum vernetzt (z. B. Kinderschutz, Kriminalpräventiver Rat)</li>
+        </ul>
+    </div>
+    <div class="arbeitszeiten_ssa_mb">
+        <h2>Meine Arbeitszeiten:</h2>
+        <p>
+            Mo. - Fr. 7:00 bis 14:00 Uhr
+        </p>
+    </div>
+    <div class="erreichen_ssa_mb">
+        <h2>Wie kann ich Sie erreichen?</h2>
+        <p>E-Mail: soz_kavelmann@gmx.de</p>
+        <p>Telefon: 03998/ 271114</p>
+        <p>Raum: 03.09</p>
+        <p>Angestellt bin ich beim Trägerwerk Soziale Dienste M-V.A</p>
+    </div>
+</div>
 <?php Layout::Footer();
