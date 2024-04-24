8c096578735fc754c1cf11026fd77f827da7a3ff
HevenStalkings <122364300+wisiboy99@users.noreply.github.com>
2024-04-24 10:08:16 +0200

    berufsorientrierung
    neu erstellt

--- VIEW/berufsorientierungPraktika.php
+++ VIEW/berufsorientierungPraktika.php
@@ -2,19 +2,21 @@
 
 <img class="LandingpagePicture" src="IMG/classroom.jpg" alt="Classroom-picture">
 
-<div style="padding-top:100px; text-align: center; padding-right: 200px; padding-left: 200px;" >
-    <a target="blank" href="https://www.arbeitsagentur.de/"><h1 style= align-text: center;> <u>Agentur für Arbeit</u></h1></a>
-        <h4>Die Agentur für Arbeit ist eine staatliche Institution in Deutschland, die sich um die Vermittlung von Arbeitsplätzen kümmert und Arbeitssuchenden bei der Jobsuche unterstützt. Sie bietet auch Berufsberatung, Weiterbildungsangebote und finanzielle Unterstützung wie Arbeitslosengeld. Ihr Ziel ist es, die Arbeitslosigkeit zu bekämpfen und die Beschäftigungssituation in Deutschland zu verbessern.</h4>  
-</div>
-
-<div style=  "text-align: center; padding-right: 200px; padding-left: 200px;">
-    <h1><u><br>Praktika</br></u></h1>
-        <h4>Praktika dienen dazu, theoretisches Wissen in der Praxis anzuwenden und praktische Erfahrungen in einem bestimmten Berufsfeld zu sammeln. Sie ermöglichen es den Teilnehmern, Einblicke in die Arbeitswelt zu gewinnen, Fähigkeiten zu entwickeln und Kontakte zu knüpfen. Praktika sind eine wichtige Möglichkeit, um Berufsorientierung zu erhalten, sich persönlich und beruflich weiterzuentwickeln sowie potenzielle Karrierewege zu erkunden. Sie helfen dabei, die Anforderungen und Erwartungen des Arbeitsmarktes besser zu verstehen und erleichtern den Übergang von der Ausbildung in das Berufsleben.</h4>
-</div>
-
-<div style= "  text-align: center; padding-right: 200px; padding-left: 200px;" >
-    <h1><u><br>Messen&Co</br></u></h1>
-        <h4>Messen und ähnliche Veranstaltungen dienen dazu, Unternehmen eine Plattform zu bieten, um ihre Produkte und Dienstleistungen einem breiten Publikum vorzustellen. Sie ermöglichen es Unternehmen, potenzielle Kunden direkt anzusprechen, neue Geschäftskontakte zu knüpfen und bestehende Beziehungen zu pflegen. Darüber hinaus bieten Messen eine Gelegenheit für Branchenexperten, sich über aktuelle Trends und Entwicklungen auszutauschen, sowie für Besucher, sich über verschiedene Produkte und Innovationen zu informieren. Letztendlich tragen Messen dazu bei, den Austausch von Ideen und Innovationen zu fördern und das Wachstum von Unternehmen und Branchen voranzutreiben.</h4>
+<div class="container text-center">
+    <div class="row">
+        <div class="col">
+            <h1 class="afa" style="background-color: #90001c; border: 1px solid black; margin-top: 50px; margin-bottom: 50px; color: white;"><u>Agentur für Arbeit</u></h1>
+                <h4 style= "text-align: left; background-color: #848d93; min-height: 700px;">Die Agentur für Arbeit ist eine staatliche Institution in Deutschland, die sich um die Vermittlung von Arbeitsplätzen kümmert und Arbeitssuchenden bei der Jobsuche unterstützt. Sie bietet auch Berufsberatung, Weiterbildungsangebote und finanzielle Unterstützung wie Arbeitslosengeld. Ihr Ziel ist es, die Arbeitslosigkeit zu bekämpfen und die Beschäftigungssituation in Deutschland zu verbessern.</h4>
+        </div>
+        <div class="col">
+            <h1 class="Praktika" style="background-color: #90001c; border: 1px solid black; margin-top: 50px; margin-bottom: 50px; color: white;"><u>Praktika</u></h1>
+                <h4 style= "text-align: left; background-color: #848d93; min-height: 700px;">Praktika dienen dazu, theoretisches Wissen in der Praxis anzuwenden und praktische Erfahrungen in einem bestimmten Berufsfeld zu sammeln. Sie ermöglichen es den Teilnehmern, Einblicke in die Arbeitswelt zu gewinnen, Fähigkeiten zu entwickeln und Kontakte zu knüpfen. Praktika sind eine wichtige Möglichkeit, um Berufsorientierung zu erhalten, sich persönlich und beruflich weiterzuentwickeln sowie potenzielle Karrierewege zu erkunden. Sie helfen dabei, die Anforderungen und Erwartungen des Arbeitsmarktes besser zu verstehen und erleichtern den Übergang von der Ausbildung in das Berufsleben.</h4>
+        </div>
+        <div class="col">
+            <h1 class="Messen" style="background-color: #90001c; border: 1px solid black; margin-top: 50px; margin-bottom: 50px; color: white;"><u>Messen&Co</u></h1>
+                <h4 style= "text-align: left; background-color: #848d93; min-height: 700px; margin-bottom: 100px;">Messen und ähnliche Veranstaltungen dienen dazu, Unternehmen eine Plattform zu bieten, um ihre Produkte und Dienstleistungen einem breiten Publikum vorzustellen. Sie ermöglichen es Unternehmen, potenzielle Kunden direkt anzusprechen, neue Geschäftskontakte zu knüpfen und bestehende Beziehungen zu pflegen. Darüber hinaus bieten Messen eine Gelegenheit für Branchenexperten, sich über aktuelle Trends und Entwicklungen auszutauschen, sowie für Besucher, sich über verschiedene Produkte und Innovationen zu informieren. Letztendlich tragen Messen dazu bei, den Austausch von Ideen und Innovationen zu fördern und das Wachstum von Unternehmen und Branchen voranzutreiben.</h4>
+        </div>
+    </div>
 </div>
 
 
