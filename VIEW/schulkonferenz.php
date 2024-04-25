8b94987fde1a7aa7291fdfd5ea528c70a3edebc7
ovekutz <144100279+ovekutz@users.noreply.github.com>
2024-04-25 09:43:14 +0200

    Update schulkonferenz.php
    Tabelle für Elternrat in Schulkonferenz eingefügt

--- VIEW/schulkonferenz.php
+++ VIEW/schulkonferenz.php
@@ -1,16 +1,26 @@
-<?php Layout::Header("Schulkonferenz"); ?>   
+<?php Layout::Header("Schulkonferenz"); ?>
 <!--Begin Mitwirkungsgremien (Ove Kutz)-->
 
 <div class="container" style="padding-top: 150px;">
     <div class="col-left">
-        <h1>penis</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam molestias doloremque laborum veritatis, maxime dolores ea adipisci qui, officiis eligendi tempore ex repellat voluptatem! Fugit.</p>
+        <h1>Schulkonferenz</h1>
+        <p>Bei Schulkonferenzen haben Schüler die Möglichkeit, bei aktuellen Themen ihre Meinung dazu zu äußern</p>
     </div>
     <div class="col-right">
-        <h1>penis</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam molestias doloremque laborum veritatis, maxime dolores ea adipisci qui, officiis eligendi tempore ex repellat voluptatem! Fugit.</p>
+        <h1>Elternrat</h1>
+        <table>
+            <tr><td>Vorsitzende:</td><td>Frau Holldorf</td></tr>
+            <tr><td>1. Stellvertreterin:</td><td>Frau Kochalsky</td></tr>
+            <tr><td>2. Stellvertreterin:</td><td>Frau Mienert</td></tr>
+        </table>
+    </div>
+    <div class="col-center">
+        <h1>Schulsprecher</h1>
+        <p>Die Schülersprecher sind Ansprechpartner für Schulleitung, Schüler, Eltern, Sekretärin, Hausmeister usw.</p>
     </div>
 
 </div>
 
 <?php Layout::Footer();
+
+// End Mitwirkungsgremien (Ove Kutz)--
