28882ca575081520ecd939705053532f4b0a7269
HevenStalkings <122364300+wisiboy99@users.noreply.github.com>
2024-04-25 09:15:50 +0200

    berufsorientierung
    Border-Radius changed

--- VIEW/berufsorientierung.php
+++ VIEW/berufsorientierung.php
@@ -5,16 +5,39 @@
 <div class="container text-center">
     <div class="row">
         <div class="col">
-            <h1 class="afa" style="background-color: #721626; border: 1px solid black; margin-top: 50px; margin-bottom: 50px; color: white;"><u>Agentur für Arbeit</u></h1>
-                <h4 style= "text-align: left; background-color: #A3A2A0; min-height: 700px;">Die Agentur für Arbeit ist eine staatliche Institution in Deutschland, die sich um die Vermittlung von Arbeitsplätzen kümmert und Arbeitssuchenden bei der Jobsuche unterstützt. Sie bietet auch Berufsberatung, Weiterbildungsangebote und finanzielle Unterstützung wie Arbeitslosengeld. Ihr Ziel ist es, die Arbeitslosigkeit zu bekämpfen und die Beschäftigungssituation in Deutschland zu verbessern.</h4>
+            <h1 class="afa"
+                style="background-color: #721626; border: 1px solid black; margin-top: 50px; margin-bottom: 50px; color: white; border-radius: 20px;">
+                <u>Agentur für Arbeit</u></h1>
+            <h4 style="text-align: left; background-color: #A3A2A0; min-height: 700px; border-radius: 20px;"><br>Die Agentur für Arbeit ist eine
+                staatliche Institution in Deutschland, die sich um die Vermittlung von Arbeitsplätzen kümmert und
+                Arbeitssuchenden bei der Jobsuche unterstützt. Sie bietet auch Berufsberatung, Weiterbildungsangebote
+                und finanzielle Unterstützung wie Arbeitslosengeld. Ihr Ziel ist es, die Arbeitslosigkeit zu bekämpfen
+                und die Beschäftigungssituation in Deutschland zu verbessern.</br></h4>
         </div>
         <div class="col">
-            <h1 class="Praktika" style="background-color: #721626; border: 1px solid black; margin-top: 50px; margin-bottom: 50px; color: white;"><u>Praktika</u></h1>
-                <h4 style= "text-align: left; background-color: #A3A2A0; min-height: 700px;">Praktika dienen dazu, theoretisches Wissen in der Praxis anzuwenden und praktische Erfahrungen in einem bestimmten Berufsfeld zu sammeln. Sie ermöglichen es den Teilnehmern, Einblicke in die Arbeitswelt zu gewinnen, Fähigkeiten zu entwickeln und Kontakte zu knüpfen. Praktika sind eine wichtige Möglichkeit, um Berufsorientierung zu erhalten, sich persönlich und beruflich weiterzuentwickeln sowie potenzielle Karrierewege zu erkunden. Sie helfen dabei, die Anforderungen und Erwartungen des Arbeitsmarktes besser zu verstehen und erleichtern den Übergang von der Ausbildung in das Berufsleben.</h4>
+            <h1 class="Praktika"
+                style="background-color: #721626; border: 1px solid black; margin-top: 50px; margin-bottom: 50px; color: white; border-radius: 20px;">
+                <u>Praktika</u></h1>
+            <h4 style="text-align: left; background-color: #A3A2A0; min-height: 700px; border-radius: 20px;"><br>Praktika dienen dazu,
+                theoretisches Wissen in der Praxis anzuwenden und praktische Erfahrungen in einem bestimmten Berufsfeld
+                zu sammeln. Sie ermöglichen es den Teilnehmern, Einblicke in die Arbeitswelt zu gewinnen, Fähigkeiten zu
+                entwickeln und Kontakte zu knüpfen. Praktika sind eine wichtige Möglichkeit, um Berufsorientierung zu
+                erhalten, sich persönlich und beruflich weiterzuentwickeln sowie potenzielle Karrierewege zu erkunden.
+                Sie helfen dabei, die Anforderungen und Erwartungen des Arbeitsmarktes besser zu verstehen und
+                erleichtern den Übergang von der Ausbildung in das Berufsleben.</br></h4>
         </div>
         <div class="col">
-            <h1 class="Messen" style="background-color: #721626; border: 1px solid black; margin-top: 50px; margin-bottom: 50px; color: white;"><u>Messen&Co</u></h1>
-                <h4 style= "text-align: left; background-color: #A3A2A0; min-height: 700px; margin-bottom: 100px;">Messen und ähnliche Veranstaltungen dienen dazu, Unternehmen eine Plattform zu bieten, um ihre Produkte und Dienstleistungen einem breiten Publikum vorzustellen. Sie ermöglichen es Unternehmen, potenzielle Kunden direkt anzusprechen, neue Geschäftskontakte zu knüpfen und bestehende Beziehungen zu pflegen. Darüber hinaus bieten Messen eine Gelegenheit für Branchenexperten, sich über aktuelle Trends und Entwicklungen auszutauschen, sowie für Besucher, sich über verschiedene Produkte und Innovationen zu informieren. Letztendlich tragen Messen dazu bei, den Austausch von Ideen und Innovationen zu fördern und das Wachstum von Unternehmen und Branchen voranzutreiben.</h4>
+            <h1 class="Messen"
+                style="background-color: #721626; border: 1px solid black; margin-top: 50px; margin-bottom: 50px; color: white; border-radius: 20px;">
+                <u>Messen&Co</u></h1>
+            <h4 style="text-align: left; background-color: #A3A2A0; min-height: 700px; margin-bottom: 100px; border-radius: 20px;"><br>Messen und
+                ähnliche Veranstaltungen dienen dazu, Unternehmen eine Plattform zu bieten, um ihre Produkte und
+                Dienstleistungen einem breiten Publikum vorzustellen. Sie ermöglichen es Unternehmen, potenzielle Kunden
+                direkt anzusprechen, neue Geschäftskontakte zu knüpfen und bestehende Beziehungen zu pflegen. Darüber
+                hinaus bieten Messen eine Gelegenheit für Branchenexperten, sich über aktuelle Trends und Entwicklungen
+                auszutauschen, sowie für Besucher, sich über verschiedene Produkte und Innovationen zu informieren.
+                Letztendlich tragen Messen dazu bei, den Austausch von Ideen und Innovationen zu fördern und das
+                Wachstum von Unternehmen und Branchen voranzutreiben.</br></h4>
         </div>
     </div>
 </div>
