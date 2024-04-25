30478fb263a968010a51f16e2b92155a54fdc668
ovekutz <144100279+ovekutz@users.noreply.github.com>
2024-04-25 14:09:43 +0200

    Update schulkonferenz.php
    Schulkonferenz abgeschlossen

--- VIEW/schulkonferenz.php
+++ VIEW/schulkonferenz.php
@@ -1,12 +1,15 @@
 <?php Layout::Header("Schulkonferenz"); ?>
 <!--Begin Mitwirkungsgremien (Ove Kutz)-->
 
-<div class="container" style="padding-top: 150px;">
-    <div class="col-left">
+<divclass="container" style="padding-top: 150px;"div>
+<div class="schulkonferenz">Schulkonferenz</div>
+    <div class="elternrat">Elternrat</div>
+    <div class="schulsprecher">Schulsprecher</div>    
+<div class="schulkonferenz"><br>
         <h1>Schulkonferenz</h1>
-        <p>Bei Schulkonferenzen haben Schüler die Möglichkeit, bei aktuellen Themen ihre Meinung dazu zu äußern</p>
+        <p>Eine Schulkonferenz ist ein Gremium, das aus Vertretern der Schulleitung, Lehrerinnen und Lehrern, Eltern sowie gegebenenfalls Schülerinnen und Schülern besteht und regelmäßig zusammenkommt, um über wichtige Angelegenheiten im Zusammenhang mit der Schule zu beraten und Entscheidungen zu treffen. Ihre Aufgaben umfassen die Diskussion von Bildungszielen, Schulprogrammen, Lehrplänen, Budgetfragen, Schulordnungen und anderen Belangen, die die Schule betreffen. Die Schulkonferenz dient als demokratisches Forum, in dem verschiedene Interessengruppen zusammenkommen, um gemeinsam an der Weiterentwicklung und Verbesserung der Schule zu arbeiten.</p>
     </div>
-    <div class="col-right">
+    <div class="elternrat">
         <h1>Elternrat</h1>
         <table>
             <tr><td>Vorsitzende:</td><td>Frau Holldorf</td></tr>
@@ -14,10 +17,11 @@
             <tr><td>2. Stellvertreterin:</td><td>Frau Mienert</td></tr>
         </table>
     </div>
-    <div class="col-center">
+<div class="schulsprecher">
+
         <h1>Schulsprecher</h1>
-        <p>Die Schülersprecher sind Ansprechpartner für Schulleitung, Schüler, Eltern, Sekretärin, Hausmeister usw.</p>
-    </div>
+        <p>Ein Schulsprecher ist eine gewählte oder ernannte Schülervertretung, die die Interessen und Anliegen ihrer Mitschülerinnen und Mitschüler gegenüber der Schulleitung, Lehrern und anderen Institutionen vertritt. Ihre Aufgaben umfassen die Förderung des Schülerengagements, die Organisation von schulischen Veranstaltungen, die Lösung von Problemen und Konflikten sowie die Förderung eines positiven und inklusiven Schulklimas. Der Schulsprecher dient als Bindeglied zwischen Schülern und der Schulverwaltung und arbeitet daran, die Schülerstimme in Entscheidungsprozessen zu stärken und die Schülergemeinschaft zu stärken.</p>
+    
 
 </div>
 
