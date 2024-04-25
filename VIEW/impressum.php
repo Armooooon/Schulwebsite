86977acea930fb02548612dfcea88d66fff8bbfb
Steffen <sutschke88@gmail.com>
2024-04-25 10:39:45 +0200

    änderung Impressum css u. php, änderung an: foerderverein css und php

--- VIEW/impressum.php
+++ VIEW/impressum.php
@@ -1,20 +1,33 @@
 <?php Layout::Header("Kontakt"); ?>
 
-    <p><strong>Impressum</strong></p>
-    <p>Schulleitung
-        <br />Frau Dr. Melanie Brüchner
-        <br />Anklamer Feld 9
-        <br />17109 Demmin
-        <br />Telefon: 03998/27110
-        <br />E-Mail: pestalozzischule@demmin.de
-        <br />Die Pestalozzi-Schule ist eine Körperschaft des Öffentlichen Rechts. Sie wird durch den Schulleiter gesetzlich vertreten.</p>
-    <p>Schulträger
-        <br />Hansestadt Demmin
-        <br />Bürgermeister: Thomas Wittkowski
-        <br />Markt 1
-        <br />17109 Demmin
-        <br />Telefon: 03998/2560
-        <br />E-Mail: hansestadt@demmin.de
-        <br />Internet: www.demmin.de</p>
+<!-- Steven hat gekocht -->
+
+<div class="container" style="padding-top: 150px;">
+    <div class="impessumalles">
+        <div class="impressum-box">
+            <h2>Impressum</h2>
+            <div class="schulleitung">
+                <h3>Schulleitung</h3>
+                <p>Frau Dr. Melanie Brüchner</p>
+                <p>Anklamer Feld 9</p>
+                <p>17109 Demmin</p>
+                <p>Telefon: 03998/27110</p>
+                <p>E-Mail: pestalozzischule@demmin.de</p>
+                <p>Die Pestalozzi-Schule ist eine Körperschaft des Öffentlichen Rechts. Sie wird durch den Schulleiter
+                    gesetzlich vertreten.</p>
+            </div>
+            <div class="schultraeger">
+                <h3>Schulträger</h3>
+                <p>Hansestadt Demmin</p>
+                <p>Bürgermeister: Thomas Wittkowski</p>
+                <p>Markt 1</p>
+                <p>17109 Demmin</p>
+                <p>Telefon: 03998/2560</p>
+                <p>E-Mail: hansestadt@demmin.de</p>
+                <p>Internet: <a href="www.demmin.de">www.demmin.de</a></p>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php Layout::Footer();
