204ba60c8113b95dd1a4528c22808e2da58d7684
Steffen <sutschke88@gmail.com>
2024-04-25 13:24:15 +0200

    hab den müll gemacht

--- VIEW/foerderverein.php
+++ VIEW/foerderverein.php
@@ -5,53 +5,53 @@
 <!-- steven hat gekocht -->
 <div class="container" style="display: block;">
     <div class="main-text-container">
-        <h1>Unser Förderverein</h1>
+        <h1 class ="ueberschriftSU">Unser Förderverein</h1>
         <div class="main-text">
-            <p>Sehr geehrte Eltern,</p>
-            <p>Werden Sie Mitglied unseres Fördervereins! Den Förderverein gibt es bereits seit dem 28.02.1997 (Datum der Gründungsversammlung). Mit einem mitgliederstarken Förderverein können wir unsere Schule, und damit unsere Kinder, noch besser unterstützen. Anträge auf Mitgliedschaft gibt es in der Schule! Der Jahresbeitrag beträgt nur 7,00 €. Sie möchten uns als Sponsor unterstützen? Eine vom Finanzamt anerkannte Spendenbescheinigung gibt es sofort.</p>
+            <p class ="textSU">Sehr geehrte Eltern,</p>
+            <p class ="textSU">Werden Sie Mitglied unseres Fördervereins! Den Förderverein gibt es bereits seit dem 28.02.1997 (Datum der Gründungsversammlung). Mit einem mitgliederstarken Förderverein können wir unsere Schule, und damit unsere Kinder, noch besser unterstützen. Anträge auf Mitgliedschaft gibt es in der Schule! Der Jahresbeitrag beträgt nur 7,00 €. Sie möchten uns als Sponsor unterstützen? Eine vom Finanzamt anerkannte Spendenbescheinigung gibt es sofort.</p>
         </div>
         <section id="vorstand">
-            <h2>Vorstand des Fördervereins</h2>
+            <h1 class ="ueberschriftSU">Vorstand des Fördervereins</h1>
             <table class="rounded-border">
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Funktion</th>
+                        <th class ="tabelleSU">Name</th>
+                        <th class ="tabelleSU">Funktion</th>
                     </tr>
                 </thead>
                 <tbody>
                     <!-- Vorstandsmitglieder auflisten -->
-                    <tr>
-                        <td>K. Kavelmann</td>
-                        <td>Vorsitzende</td>
+                    <tr class ="tabelleinhalt1SU">
+                        <td class ="tabelleninhalt2SU">K. Kavelmann</td>
+                        <td class ="tabelleninhalt2SU">Vorsitzende</td>
                     </tr>
-                    <tr>
-                        <td>F. Reppin</td>
-                        <td>stellv. Vorsitzender</td>
+                    <tr class ="tabelleinhalt1SU">
+                        <td class ="tabelleninhalt2SU">F. Reppin</td>
+                        <td class ="tabelleninhalt2SU">stellv. Vorsitzender</td>
                     </tr>
-                    <tr>
-                        <td>A. Fingerloos</td>
-                        <td>Schriftführer</td>
+                    <tr class ="tabelleinhalt1SU">
+                        <td class ="tabelleninhalt2SU">A. Fingerloos</td>
+                        <td class ="tabelleninhalt2SU">Schriftführer</td>
                     </tr>
-                    <tr>
-                        <td>A. Garling</td>
-                        <td>Kassenwart</td>
+                    <tr class ="tabelleinhalt1SU">
+                        <td class ="tabelleninhalt2SU">A. Garling</td>
+                        <td class ="tabelleninhalt2SU">Kassenwart</td>
                     </tr>
-                    <tr>
-                        <td>K. Weißhoff</td>
-                        <td>1. Kassenprüfer</td>
+                    <tr class ="tabelleinhalt1SU">
+                        <td class ="tabelleninhalt2SU">K. Weißhoff</td>
+                        <td class ="tabelleninhalt2SU">1. Kassenprüfer</td>
                     </tr>
-                    <tr>
-                        <td>R. Wiese</td>
-                        <td>2. Kassenprüfer</td>
+                    <tr class ="tabelleinhalt1SU">
+                        <td class ="tabelleninhalt2SU">R. Wiese</td>
+                        <td class ="tabelleninhalt2SU">2. Kassenprüfer</td>
                     </tr>
-                    <tr>
-                        <td>S. Becker</td>
-                        <td>1. Beisitzer</td>
+                    <tr class ="tabelleinhalt1SU">
+                        <td class ="tabelleninhalt2SU">S. Becker</td>
+                        <td class ="tabelleninhalt2SU">1. Beisitzer</td>
                     </tr>
-                    <tr>
-                        <td>L. Paß-Schober</td>
-                        <td>2. Beisitzer</td>
+                    <tr class ="tabelleinhalt1SU">
+                        <td class ="tabelleninhalt2SU">L. Paß-Schober</td>
+                        <td class ="tabelleninhalt2SU">2. Beisitzer</td>
                     </tr>
                     <!-- Weitere Vorstandsmitglieder hier einfügen -->
                 </tbody>
@@ -59,22 +59,22 @@
         </section>
         <div class="additional-text">
             <!-- Dank an Sponsoren -->
-            <p>Wir bedanken uns ganz herzlich bei unseren Sponsoren - ganz besonders bei der Firma Kroos.</p>
+            <p class ="textSU">Wir bedanken uns ganz herzlich bei unseren Sponsoren - ganz besonders bei der Firma Kroos.</p>
             <!-- Projekte und Unterstützungen auflisten -->
-            <p>Im Jahr 2022 wurde das Projekt "Footballcamp" vom Förderverein unterstützt.</p>
-            <p>2023: - Finanzierung des Apfelprojekts in Klasse 3</p>
-            <h2>Mitglieder</h2>
+            <p class ="textSU">Im Jahr 2022 wurde das Projekt "Footballcamp" vom Förderverein unterstützt.</p>
+            <p class ="textSU">2023: - Finanzierung des Apfelprojekts in Klasse 3</p>
+            <h1 class ="ueberschriftSU"> Mitglied </h1>
             <div class="member-info-container text-center">
                 <div class="row">
                     <div class="col">
-                        <h4>Antrag</h4>
-                        <p>Falls Sie Mitglied werden möchten, können Sie hier den Antrag</a> herunterladen.</p>
+                    <h1 class ="ueberschriftSU"> </h1>
+                    <p class ="textSU">Falls Sie Mitglied werden möchten, können Sie hier den Antrag</a> herunterladen.</p>
                         <!-- Button für den Antrag -->
                         <button class="accent-button" onclick="window.open('link_zum_antrag', '_blank')">Antrag herunterladen</button>
                     </div>
                     <div class="col">
-                        <h4>Satzung</h4>
-                        <p>Sie können auch die <a href="link_zur_satzung">Satzung</a> des Fördervereins einsehen.</p>
+                    <h1 class ="ueberschriftSU"> Satzung </h1>
+                    <p class ="textSU">Sie können auch die <a href="link_zur_satzung">Satzung</a> des Fördervereins einsehen.</p>
                     </div>
                 </div>
             </div>
