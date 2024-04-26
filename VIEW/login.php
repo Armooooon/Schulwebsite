615d52aa3e89fcd5eedd0fc7083ee8cf3c9eade2
Aron <halaoui.aron@gmail.com>
2024-04-26 17:58:59 +0200

    Created the Login site and styled it

---
+++ VIEW/login.php
@@ -0,0 +1,30 @@
+
+<?php
+
+require_once 'CLASS/Layout.php';
+
+$cssFiles = ['login.css'];
+
+Layout::Header("Login", $cssFiles);
+
+?>
+
+<!-- Login Form von Aron Halaoui -->
+<div class="containerLogin">
+  <div class="login-form">
+    <h2>Login</h2>
+    <form action="#" method="POST">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username" required>
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required>
+      </div>
+      <button type="submit" class="btn">Login</button>
+    </form>
+  </div>
+</div>
+
+<?php Layout::Footer();
