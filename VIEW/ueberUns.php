4d41d44486d9dd63147255bb900a88245483c47f
Samir <122364498+Stileexx@users.noreply.github.com>
2024-05-02 14:32:45 +0200

    neue section Über Uns

--- VIEW/ueberUns.php
+++ VIEW/ueberUns.php
@@ -1,32 +1,65 @@
 <?php Layout::Header("Über uns"); ?>
 <!-- Samir Abdul -->
 <!-- Container with padding top and gap between elements -->
-<div class="container" style="padding-top: 150px; gap:10px;">
-  <!-- Row container -->
-  <div class="row">
-    <!-- Header with centered text -->
-    <h1 class="text-center">Über uns</h1>
-    <!-- Column with padding -->
-    <div class="col" style="padding: 20px;">
-      <!-- Text container -->
-      <div class="ueberUnsText">
-        <!-- Paragraph with school description -->
-        <p>Willkommen auf der Webseite der Pestalozzi-Schule Demmin, einer Einrichtung, die sich dem ganzheitlichen Lernen und der persönlichen Entwicklung jedes einzelnen Schülers widmet. Unsere Schule, benannt nach dem Pädagogen Johann Heinrich Pestalozzi, steht für eine Bildungsphilosophie, die Wissen, Herz und Hand in Einklang bringt.
+<div class="container" style="padding-top: 8rem; gap:10px; background:var(--color2);">
+  <section>
+    <!-- Row container -->
+    <div class="row">
+      <!-- Header with centered text -->
+      <h1 class="text-center">Über uns</h1>
+      <!-- Column with padding -->
+      <div class="col" style="padding: 20px;">
+        <!-- Text container -->
+        <div class="ueberUnsText">
+          <!-- Paragraph with school description -->
+          <p>Willkommen auf der Webseite der Pestalozzi-Schule Demmin, einer Einrichtung, die sich dem ganzheitlichen Lernen und der persönlichen Entwicklung jedes einzelnen Schülers widmet. Unsere Schule, benannt nach dem Pädagogen Johann Heinrich Pestalozzi, steht für eine Bildungsphilosophie, die Wissen, Herz und Hand in Einklang bringt.
 
-          Seit unserer Gründung im Jahr 1975 verfolgen wir das Ziel, eine umfassende Bildung zu bieten, die die Schüler nicht nur akademisch, sondern auch sozial und emotional stärkt. Unsere Lehrkräfte sind hochqualifiziert und engagiert, um eine Lernumgebung zu schaffen, die innovativ, unterstützend und herausfordernd ist.
+            Seit unserer Gründung im Jahr 1975 verfolgen wir das Ziel, eine umfassende Bildung zu bieten, die die Schüler nicht nur akademisch, sondern auch sozial und emotional stärkt. Unsere Lehrkräfte sind hochqualifiziert und engagiert, um eine Lernumgebung zu schaffen, die innovativ, unterstützend und herausfordernd ist.
 
-          Die Pestalozzi-Schule bietet eine breite Palette von Lehrplänen in den Bereichen Sprachen, Mathematik, Naturwissenschaften, Kunst und Sport. Wir sind stolz auf unsere moderne Ausstattung, die es den Schülern ermöglicht, in einem dynamischen und interaktiven Umfeld zu lernen.
+            Die Pestalozzi-Schule bietet eine breite Palette von Lehrplänen in den Bereichen Sprachen, Mathematik, Naturwissenschaften, Kunst und Sport. Wir sind stolz auf unsere moderne Ausstattung, die es den Schülern ermöglicht, in einem dynamischen und interaktiven Umfeld zu lernen.
 
-          Neben dem akademischen Programm legen wir großen Wert auf die soziale Entwicklung und das Gemeinschaftsgefühl. Unsere zahlreichen außerschulischen Aktivitäten, wie Sportteams, Musikgruppen und verschiedene Clubs, fördern Teamarbeit und persönliche Entfaltung.
+            Neben dem akademischen Programm legen wir großen Wert auf die soziale Entwicklung und das Gemeinschaftsgefühl. Unsere zahlreichen außerschulischen Aktivitäten, wie Sportteams, Musikgruppen und verschiedene Clubs, fördern Teamarbeit und persönliche Entfaltung.
 
-          Wir laden Sie ein, mehr über unsere Schule zu erfahren und Teil unserer Gemeinschaft zu werden.</p>
+            Wir laden Sie ein, mehr über unsere Schule zu erfahren und Teil unserer Gemeinschaft zu werden.</p>
+        </div>
+      </div>
+      <!-- Column with padding -->
+      <div class="col" style="padding: 20px;">
+        <!-- Image with fixed width and alt text -->
+        <img src="./IMG/pestaloziSchulfoto.jpg" style="padding:2px; width: 100%; border: 3px solid var(--color1);" alt="Foto der Schule">
       </div>
     </div>
-    <!-- Column with padding -->
-    <div class="col" style="padding: 20px;">
-      <!-- Image with fixed width and alt text -->
-      <img src="./IMG/pestaloziSchulfoto.jpg" style="width: 100%; border: 3px solid var(--color1);" alt="Foto der Schule">
+  </section>
+  <hr>
+  <section>
+    <!-- Row container -->
+    <div class="row">
+      <!-- Header with centered text -->
+      <h1 class="text-center">Grußwort von Dr. Melanie Brüchner</h1>
+            <!-- Column with padding -->
+            <div class="col" style="padding: 20px;">
+        <!-- Image with fixed width and alt text -->
+        <img src="./IMG/FrBruechner.jpg" style="padding:2px; width: 100%; border: 3px solid var(--color1);" alt="Foto der Schulleiterin">
+      </div>
+      <!-- Column with padding -->
+      <div class="col" style="padding: 20px;">
+        <!-- Text container -->
+        <div class="ueberUnsText">
+          <!-- Paragraph with school description -->
+          <p>Willkommen auf der Webseite der Pestalozzi-Schule Demmin, einer Einrichtung, die sich dem ganzheitlichen Lernen und der persönlichen Entwicklung jedes einzelnen Schülers widmet. Unsere Schule, benannt nach dem Pädagogen Johann Heinrich Pestalozzi, steht für eine Bildungsphilosophie, die Wissen, Herz und Hand in Einklang bringt.
+
+            Seit unserer Gründung im Jahr 1975 verfolgen wir das Ziel, eine umfassende Bildung zu bieten, die die Schüler nicht nur akademisch, sondern auch sozial und emotional stärkt. Unsere Lehrkräfte sind hochqualifiziert und engagiert, um eine Lernumgebung zu schaffen, die innovativ, unterstützend und herausfordernd ist.
+
+            Die Pestalozzi-Schule bietet eine breite Palette von Lehrplänen in den Bereichen Sprachen, Mathematik, Naturwissenschaften, Kunst und Sport. Wir sind stolz auf unsere moderne Ausstattung, die es den Schülern ermöglicht, in einem dynamischen und interaktiven Umfeld zu lernen.
+
+            Neben dem akademischen Programm legen wir großen Wert auf die soziale Entwicklung und das Gemeinschaftsgefühl. Unsere zahlreichen außerschulischen Aktivitäten, wie Sportteams, Musikgruppen und verschiedene Clubs, fördern Teamarbeit und persönliche Entfaltung.
+
+            Wir laden Sie ein, mehr über unsere Schule zu erfahren und Teil unserer Gemeinschaft zu werden.</p>
+        </div>
+      </div>
+
     </div>
-  </div>
+  </section>
+
 </div>
 <?php Layout::Footer();
