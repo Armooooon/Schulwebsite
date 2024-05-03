166a83e86a274721a446f4f9a77079a87c87c81b
Samir <122364498+Stileexx@users.noreply.github.com>
2024-05-03 11:51:08 +0200

    Über uns Logos

--- VIEW/ueberUns.php
+++ VIEW/ueberUns.php
@@ -1,7 +1,7 @@
 <?php Layout::Header("Über uns"); ?>
 <!-- Samir Abdul -->
 <!-- Container with padding top and gap between elements -->
-<div class="container" style="padding-top: 8rem; gap:10px; background:var(--color2);">
+<div class="container" style="padding-top: 8rem; gap:10px;">
   <section>
     <!-- Row container -->
     <div class="row">
@@ -26,7 +26,7 @@
       <!-- Column with padding -->
       <div class="col" style="padding: 20px;">
         <!-- Image with fixed width and alt text -->
-        <img src="./IMG/pestaloziSchulfoto.jpg" style="padding:2px; width: 100%; border: 3px solid var(--color1);" alt="Foto der Schule">
+        <img src="./IMG/pestaloziSchulfoto.jpg" style="padding:2px; width: 100%; border: 3px solid var(--color1);" alt="Foto der Schule" loading="lazy">
       </div>
     </div>
   </section>
@@ -39,7 +39,7 @@
       <!-- Column with padding -->
       <div class="col" style="padding: 20px;">
         <!-- Image with fixed width and alt text -->
-        <img src="./IMG/FrBruechner.jpg" style="padding:2px; width: 100%; border: 3px solid var(--color1);" alt="Foto der Schulleiterin">
+        <img src="./IMG/FrBruechner.jpg" style="padding:2px; width: 100%; border: 3px solid var(--color1);" alt="Foto der Schulleiterin" loading="lazy">
       </div>
       <!-- Column with padding -->
       <div class="col" style="padding: 20px;">
@@ -50,7 +50,7 @@
 
             Seit unserer Gründung im Jahr 1975 verfolgen wir das Ziel, eine umfassende Bildung zu bieten, die die Schüler nicht nur akademisch, sondern auch sozial und emotional stärkt. Unsere Lehrkräfte sind hochqualifiziert und engagiert, um eine Lernumgebung zu schaffen, die innovativ, unterstützend und herausfordernd ist.
 
-            Die Pestalozzi-Schule bietet eine breite Palette von Lehrplänen in den Bereichen Sprachen, Mathematik, Naturwissenschaften, Kunst und Sport. Wir sind stolz auf unsere moderne Ausstattung, die es den Schülern ermöglicht, in einem dynamischen und interaktiven Umfeld zu lernen.
+            Die P estalozzi-Schule bietet eine breite Palette von Lehrplänen in den Bereichen Sprachen, Mathematik, Naturwissenschaften, Kunst und Sport. Wir sind stolz auf unsere moderne Ausstattung, die es den Schülern ermöglicht, in einem dynamischen und interaktiven Umfeld zu lernen.
 
             Neben dem akademischen Programm legen wir großen Wert auf die soziale Entwicklung und das Gemeinschaftsgefühl. Unsere zahlreichen außerschulischen Aktivitäten, wie Sportteams, Musikgruppen und verschiedene Clubs, fördern Teamarbeit und persönliche Entfaltung.
 
@@ -60,33 +60,33 @@
 
     </div>
   </section>
-  <hr>
-  <section>
-    <div class="row text-center" style="padding: 1rem;">
-      <!-- Icon 1 -->
-      <div class="col">
-        <i class="bi bi-people-fill" style="font-size: 2rem;"></i>
-        <p>Zusammenarbeit und Kommunikation</p>
-        <!-- <p>Besonders wichtig ist mir eine gute, vertrauensvolle und wertschätzende Zusammenarbeit und Kommunikation zwischen Lehrkräften, Schülerinnen und Schülern und Eltern. Dabei sollte das Lern- und Arbeitsklima geprägt sein von gegenseitiger Wertschätzung, Hilfsbereitschaft, Transparenz und Respekt, ein Lern- und Arbeitsklima, in dem sich alle verwirklichen und wohlfühlen können.</p> -->
-      </div>
-      <!-- Icon 2 -->
-      <div class="col">
-        <i class="bi bi-hand-thumbs-up-fill" style="font-size: 2rem;"></i>
-        <p>Förderung und Unterstützung</p>
-      </div>
-      <!-- Icon 3 -->
-      <div class="col">
-        <i class="bi bi-house-heart-fill" style="font-size: 2rem;"></i>
-        <p>Werte und Gemeinschaft</p>
-      </div>
-      <!-- Icon 4 -->
-      <div class="col">
-        <i class="bi bi-lightbulb-fill" style="font-size: 2rem;"></i>
-        <p>Innovation und Entwicklung</p>
-      </div>
-    </div>
-  </section>
+
 
 
 </div>
+<section style="background:var(--color2);">
+  <div class="row text-center" style="padding: 1rem;">
+    <!-- Icon 1 -->
+    <div class="col">
+      <i class="bi bi-people-fill" style="font-size: 2rem; color:var(--color1);"></i>
+      <p>Zusammenarbeit und Kommunikation</p>
+      <!-- <p>Besonders wichtig ist mir eine gute, vertrauensvolle und wertschätzende Zusammenarbeit und Kommunikation zwischen Lehrkräften, Schülerinnen und Schülern und Eltern. Dabei sollte das Lern- und Arbeitsklima geprägt sein von gegenseitiger Wertschätzung, Hilfsbereitschaft, Transparenz und Respekt, ein Lern- und Arbeitsklima, in dem sich alle verwirklichen und wohlfühlen können.</p> -->
+    </div>
+    <!-- Icon 2 -->
+    <div class="col">
+      <i class="bi bi-hand-thumbs-up-fill" style="font-size: 2rem; color:var(--color1);"></i>
+      <p>Förderung und Unterstützung</p>
+    </div>
+    <!-- Icon 3 -->
+    <div class="col">
+      <i class="bi bi-house-heart-fill" style="font-size: 2rem; color:var(--color1);"></i>
+      <p>Werte und Gemeinschaft</p>
+    </div>
+    <!-- Icon 4 -->
+    <div class="col">
+      <i class="bi bi-lightbulb-fill" style="font-size: 2rem; color:var(--color1);"></i>
+      <p>Innovation und Entwicklung</p>
+    </div>
+  </div>
+</section>
 <?php Layout::Footer();
